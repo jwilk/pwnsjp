989133d9526bbd20047ed6d978c06507c1e3b47a
Jakub Wilk <jwilk@jwilk.net>
2008-07-31 17:40:01 +0200

    Use `©` istead of `(c)` or `(C)`.

--- terminfo.h
+++ terminfo.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2005 Jakub Wilk
+/* Copyright © 2005 Jakub Wilk
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published 
  * by the Free Software Foundation.
