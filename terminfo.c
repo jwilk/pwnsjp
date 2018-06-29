jwilk/pwnsjp

commit b671a1850729171de8f763cd54bae43a7b4a88e7

terminfo: use tiparm() instead of tparm().
X/Open Curses prototypes tparm() with a fixed number of parameters,
rather than a variable argument list.

terminfo.c
@@ -91,7 +91,7 @@ void term_init(void)
   if (*s0 != '\0')
     for (j = 0; j < 8; j++)
     {
-      term_setaf[j] = tparm(s0, j);
+      term_setaf[j] = tiparm(s0, j);
       term_setaf[j] = str_clone(term_setaf[j] == NULL ? "" : term_setaf[j]);
       if (term_setaf[j] == NULL)
         term_setaf[j] = str_clone("");

commit bc8b6dbc138b2e16b978c4497a48708b9b0c963d

terminfo: replace another instance of tparm() with tiparm().
Fixes: commit b671a1850729171de8f763cd54bae43a7b4a88e7

terminfo.c
@@ -104,7 +104,7 @@ void term_init(void)
   if (*s0 != '\0')
     for (j = 0; j < 8; j++)
     {
-      term_setab[j] = tparm(s0, j);
+      term_setab[j] = tiparm(s0, j);
       term_setab[j] = str_clone(term_setab[j] == NULL ? "" : term_setab[j]);
       if (term_setab[j] == NULL)
         term_setab[j] = str_clone("");